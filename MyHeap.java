c66031d7eb9708c14a9fc8fc2e57bcf0ad14cc71
Jeremy Lee <jlee00@stuy.edu>
2019-05-02 09:05:02 -0400

    Added exceptions

--- MyHeap.java
+++ MyHeap.java
@@ -1,3 +1,24 @@
 public class MyHeap{
-  
+  private static void swap(int[] data, int a, int b){
+    int temp = data[a];
+    data[a] = data[b];
+    data[b] = temp;
+  }
+  private static void pushDown(int[] data,int size,int index){
+    if (size == 0){
+      throw new IllegalArgumentException("Empty array");
+    }
+    if (index >= size || index < 0){
+      throw new IllegalArgumentException("Index out of bounds");
+    }
+  }
+  private static void pushUp(int[] data,int index){
+
+  }
+  public static void heapify(int[] data){
+
+  }
+  public static void heapsort(int[] data){
+
+  }
 }
