d1816e75ddc5285dc00745882632414e9d9a61b2
Jeremy Lee <jlee00@stuy.edu>
2019-05-02 13:47:23 -0400

    Fixed pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyHeap{
   private static void swap(int[] data, int a, int b){
     int temp = data[a];
@@ -12,20 +13,30 @@ public class MyHeap{
       throw new IllegalArgumentException("Index out of bounds");
     }
     try{
-      if (size == 2){
-        int lowerIndex = index + 1;
-        if (data[lowerIndex] > data[index]){
-          swap(data, index, lowerIndex);
+      boolean pushed = false;
+      while (!pushed){
+        int a = 2 * index + 1;
+        int b = 2 * index + 2;
+        if (a >= size && b >= size){
+          pushed = true;
+        } else {
+          int temp = data[index];
+          if (a < size){
+            temp = Math.max(temp, data[a]);
+          }
+          if (b < size){
+            temp = Math.max(temp, data[b]);
+          }
+          if (temp == data[index]){
+            pushed = true;
+          } else if (a < size && temp == data[a]){
+            swap(data, a, index);
+            index = a;
+          } else {
+            swap(data, b, index);
+            index = b;
+          }
         }
-      }
-      int a = 2 * index + 1;
-      int b = 2 * index + 2;
-      if (data[a] > data[b] && data[a] > data[index]){
-        swap(data, a, index);
-        pushDown(data, size, a);
-      } else if (data[b] >= data[a] && data[b] > data[index]){
-        swap(data, b, index);
-        pushDown(data, size, b);
       }
     } catch(ArrayIndexOutOfBoundsException e){
       throw new ArrayIndexOutOfBoundsException();
@@ -45,15 +56,15 @@ public class MyHeap{
   public static void toString(int[] data){
     String ans = "[";
     for (int i=0; i<data.length-1; i++){
-      ans += data[i];
+      ans += data[i] + ",";
     }
     ans += data[data.length - 1] + "]";
     System.out.println(ans);
   }
 
-  public static void main(int[] args){
-    int[] data = {16,14,10,8,7,9,3,2,4,1};
-    pushDown(data, data.length, 0);
-
+  public static void main(String[] args){
+    int[] data = {16,1,10,8,7,9,3,2,4,14};
+    pushDown(data, data.length, 1);
+    toString(data);
   }
 }
