692db1053f4a59f0a24be1b0d235fae7e04a7f72
Jeremy Lee <jlee00@stuy.edu>
2019-05-02 09:11:52 -0400

    wrote pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -11,7 +11,27 @@ public class MyHeap{
     if (index >= size || index < 0){
       throw new IllegalArgumentException("Index out of bounds");
     }
+    try{
+      if (size == 2){
+        int lowerIndex = index + 1;
+        if (data[lowerIndex] > data[index]){
+          swap(data, index, lowerIndex);
+        }
+      }
+      int a = 2 * index + 1;
+      int b = 2 * index + 2;
+      if (data[a] > data[b] && data[a] > data[index]){
+        swap(data, a, index);
+        pushDown(data, size, a);
+      } else if (data[b] >= data[a] && data[b] > data[index]){
+        swap(data, b, index);
+        pushDown(data, size, b);
+      }
+    } catch(ArrayIndexOutOfBoundsException e){
+      throw new ArrayIndexOutOfBoundsException();
+    }
   }
+
   private static void pushUp(int[] data,int index){
 
   }
@@ -20,5 +40,9 @@ public class MyHeap{
   }
   public static void heapsort(int[] data){
 
+  }
+
+  public static void main(int[] args){
+    
   }
 }
